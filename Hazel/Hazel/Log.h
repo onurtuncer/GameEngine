00c8d1306504516131be0da859eb9f339f971dfe
Onur Tuncer <onur.tuncer@melina-aero.com>
2025-10-31 20:37:38 +0300

    adding logger as well

---
+++ Hazel/Hazel/Log.h
@@ -0,0 +1,18 @@
+#pragma once 
+
+#include <spdlog/spdlog.h>
+
+namespace Hazel {
+
+    class Log {
+        public:
+            static void Init();
+
+            inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+            inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+       private:
+            static std::shared_ptr<spdlog::logger> s_CoreLogger;
+            static std::shared_ptr<spdlog::logger> s_ClientLogger;
+    };
+
+}
