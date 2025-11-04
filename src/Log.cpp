36c581c9207d436ff43982eb677f7a8a66fe09d5
Onur Tuncer <onur.tuncer@milteksan.com>
2025-11-04 15:34:26 +0300

    Basic logging complete

--- src/Log.cpp
+++ src/Log.cpp
@@ -1,18 +1,20 @@
 #include "Hazel/Log.h"
 
+#include <spdlog/sinks/stdout_color_sinks.h>  // <-- REQUIRED for stdout_color_mt
+
 namespace Hazel {
 
-std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
-std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+    std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+    std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
 
-void Log::Init() {
-    spdlog::set_pattern("%^[%T] %n: %v%$");
-    s_CoreLogger = spdlog::stdout_color_mt("HAZEL");
-    s_CoreLogger->set_level(spdlog::level::trace);
+    void Log::Init() {
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+        s_CoreLogger = spdlog::stdout_color_mt("HAZEL");
+        s_CoreLogger->set_level(spdlog::level::trace);
 
-    s_ClientLogger = spdlog::stdout_color_mt("APP");
-    s_ClientLogger->set_level(spdlog::level::trace);
+        s_ClientLogger = spdlog::stdout_color_mt("APP");
+        s_ClientLogger->set_level(spdlog::level::trace);
 
-   
+    }
 
-}
+}  // namespace Hazel
